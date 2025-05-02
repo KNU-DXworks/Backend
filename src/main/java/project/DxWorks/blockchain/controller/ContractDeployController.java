b3083adaddcbf75acca5c2bba096d8895e0c1aec
한재준 <gkswowns25@naver.com>
2025-05-02 20:13:32 +0900

    feat: inbody

--- src/main/java/project/DxWorks/blockchain/controller/ContractDeployController.java
+++ src/main/java/project/DxWorks/blockchain/controller/ContractDeployController.java
@@ -39,8 +39,8 @@ public class ContractDeployController {
         return contractDeployService.addInbody(requestDto);
     }
 
-    @GetMapping("test/inbody")
-    public List<InbodyDto> getInbody() throws IOException {
-        return contractDeployService.getInbody();
+    @GetMapping("test/inbody/{privateKey}")
+    public List<InbodyDto> getInbody(@PathVariable String userPrivateKey) throws IOException {
+        return contractDeployService.getInbody(userPrivateKey);
     }
 }
