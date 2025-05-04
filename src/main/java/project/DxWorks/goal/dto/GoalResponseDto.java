132749794f87014384e2915a5c2f13e3c2b0c79f
kang-jaeh <1479242@gmail.com>
2025-05-04 15:25:00 +0900

    feat: goal

--- src/main/java/project/DxWorks/goal/dto/GoalResponseDto.java
+++ src/main/java/project/DxWorks/goal/dto/GoalResponseDto.java
@@ -1,42 +1,25 @@
-package project.DxWorks.goal.entity;
+package project.DxWorks.goal.dto;
 
-import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Entity
 @Getter
 @Setter
-public class Goal {
+@Data
+public class GoalResponseDto {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "goal_id")
     private Long goalId;
-
-    @Column(name = "created_date")
     private LocalDateTime createdDate;
-
     private Double weight;
-
     private Double muscle;
-
     private Double fat;
-
     private Double bmi;
-
     private Double arm;
-
     private Double body;
-
     private Double leg;
-
-    @Column(name = "goal_group")
     private Double goalGroup;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    private Long userId;
 }
