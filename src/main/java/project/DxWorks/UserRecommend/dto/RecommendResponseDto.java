71998042dbfbf15a4ef040dc3279ec92e96a128b
hyunsu1004 <chlgustn3680@naver.com>
2025-05-23 02:56:58 +0900

    추천 사용자 로직 구현 완료, 서버에서 test해봐야함

--- src/main/java/project/DxWorks/UserRecommend/dto/RecommendResponseDto.java
+++ src/main/java/project/DxWorks/UserRecommend/dto/RecommendResponseDto.java
@@ -1,11 +1,13 @@
 package project.DxWorks.UserRecommend.dto;
 
 
+import java.util.List;
 
 public record RecommendResponseDto(
-    long userId,
-    String userName,
-    String profileFile,
-    String prevType,
-    String bodyType
+        Long userId,
+        String userName,
+        String profileFile,
+        String prevType,
+        String bodyType,
+        List<RecommendUsersDto> recommendUser //추천 사용자 리스트
 ){}
--- src/main/java/project/DxWorks/UserRecommend/service/BigQueryService.java
+++ src/main/java/project/DxWorks/UserRecommend/service/BigQueryService.java
@@ -15,41 +15,12 @@ public class BigQueryService {
 
     private final BigQuery bigQuery = BigQueryOptions.getDefaultInstance().getService();
     private final String datasetName = "inbody_vectors";
-    private final String tableName = "embedding_vector";
+    private final String tableName = "inbody_embeddings";
     private final View error;
 
     public BigQueryService(View error) {
         this.error = error;
     }
 
-    public void saveEmbedding(long userId, EmbeddingRequestDto dto){
-        Map<String, Object> rowContent = new HashMap<>();
-        rowContent.put("userId", userId);
-        rowContent.put("created_time", dto.vector().get(0));
-        rowContent.put("gender_encoded", dto.vector().get(1));
-        rowContent.put("height", dto.vector().get(2));
-        rowContent.put("weight", dto.vector().get(3));
-        rowContent.put("muscle", dto.vector().get(4));
-        rowContent.put("fat", dto.vector().get(5));
-        rowContent.put("bmi", dto.vector().get(6));
-        rowContent.put("body_type_encoded", dto.vector().get(7));
-        rowContent.put("arm_grade_encoded", dto.vector().get(8));
-        rowContent.put("body_grade_encoded", dto.vector().get(9));
-        rowContent.put("leg_grade_encoded", dto.vector().get(10));
 
-
-        //BigQuery에 실제로 데이터를 저장하는 요청(insertAll) 실행해야함.
-        //TODO : BIGQUERY 유료 계정 업그레이드 해야함.(Streaming insert x)
-        InsertAllRequest insertAllRequest = InsertAllRequest.newBuilder(datasetName, tableName)
-                .addRow(rowContent).build();
-
-        InsertAllResponse response = bigQuery.insertAll(insertAllRequest);
-
-        if(response.hasErrors()){
-            response.getInsertErrors().forEach((key,error) -> {
-                System.err.println("삽입 중 오류 발생" + key + ": " + error);
-            });
-        }
-        throw new RuntimeException("BigQuery insert failed.");
-    }
 }
