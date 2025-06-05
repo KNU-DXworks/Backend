f0208f6b312d49387ef8744ddf9d0a4844cfde6c
한재준 <gkswowns25@naver.com>
2025-06-05 23:03:11 +0900

    hotfix: base constructor

--- src/main/java/project/DxWorks/transaction/dto/CheckTransactionRequestDto.java
+++ src/main/java/project/DxWorks/transaction/dto/CheckTransactionRequestDto.java
@@ -2,9 +2,11 @@ package project.DxWorks.transaction.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CheckTransactionRequestDto {
     private String userName;             // 구매자 텔레그램 userName
 }
